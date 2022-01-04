7ccd4f1e69ed470a0de6bffd21698e4e96321328
severinRobert <sevrobe21@gmail.com>
2022-01-04 14:12:17 +0100

    Add Dictionary class and meteo for the day working

--- lib/dictionary.py
+++ lib/dictionary.py
@@ -1,11 +1,36 @@
 from larousse_api import larousse
 from lib.command import Command
+import requests
+import re
+import unicodedata
+from bs4 import BeautifulSoup
 
 
 class Dictionary(Command):
 
-    def get_definition(self, word:list):
-        definitions = larousse.get_definitions(" ".join(word))
-        for definition in definitions:
-            print(definition)
-        print("Source : Larousse\n")
+    def __init__(self, user: int, word: str) -> None:
+        super().__init__(user)
+        self.word = word
+        self.__definitions = self.get_definitions(word)
+
+    def get_definitions(self, word):
+        """
+        :param word: The word whose definition you are looking for
+        :return: A list containing all the definitions of word
+        """
+        url = "https://www.larousse.fr/dictionnaires/francais/" + word.lower()
+        soup = BeautifulSoup(requests.get(url=url).text, 'html.parser')
+        for ul in soup.find_all('ul'):
+            if ul.get('class') is not None and 'Definitions' in ul.get('class'):
+                return [unicodedata.normalize("NFKD", re.sub("<.*?>", "", str(li))) for li in ul.find_all('li')]
+        for ul in soup.find_all('ul'):
+            if ul.get('class') is None:
+                return [f"Le mot '{self.word}' n'a pas été trouvé, mots similaires :"] + [a.string for a in ul.find_all('a')]
+        return []
+
+    def __str__(self) -> str:
+        str_to_print = ""
+        for definition in self.__definitions:
+            str_to_print += f'{definition}\n'
+        str_to_print += "Source : Larousse\n"
+        return str_to_print
--- lib/weather.py
+++ lib/weather.py
@@ -10,15 +10,13 @@ LANG = "fr"
 
 
 class Weather(Command):
-    def __init__(self, user: int, date: datetime, location: str, hour: int = None, day: str = None, weather: json = None) -> None:
-        super().__init__(user, date)
+    def __init__(self, user: int, location: str, hour: int = None, day: str = None, weather: json = None) -> None:
+        super().__init__(user)
         self.__location = location
         self.__hour = hour
         self.__day = day
         self.__weather = weather
-
-    def __str__(self) -> str:
-        pass
+        self.__error = ""
 
     def get_weather(self) -> None:
         """Cherche les données météo à l'heure demandée
@@ -28,10 +26,11 @@ class Weather(Command):
         try:
             self.__weather = requests.get(request).json()
             if str(self.__weather["cod"]) == '404':
-                print("La ville indiquée n'a pas été trouvée.")
+                self.__error = "La ville indiquée n'a pas été trouvée."
             elif str(self.__weather["cod"]) == '200':
                 latitude = self.__weather['coord']['lat']
                 longitude = self.__weather['coord']['lon']
+                self.__location = f"{self.__weather['name']}, {self.__weather['sys']['country']} "
                 # si l'utilisateur indique une heure/jour
                 if self.__hour is not None:
                     self.__weather = self.__get_weather_hourly(latitude, longitude)
@@ -45,7 +44,6 @@ class Weather(Command):
                     data = requests.get(f'https://api.openweathermap.org/data/2.5/onecall?lat={latitude}&lon={longitude}&exclude={exclude}&appid={WEATHER_APIKEY}&lang={LANG}&units=metric').json()
 
                     self.__weather = data["current"]
-                print(self)
 
         except requests.exceptions.HTTPError as errh:
             print("Http Error:", errh)
@@ -96,28 +94,31 @@ class Weather(Command):
         exclude = 'current,minutely,hourly'
         data = requests.get(
             f'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={exclude}&appid={WEATHER_APIKEY}&lang={LANG}&units=metric').json()
-
         # temps actuel en secondes
         current_time = int(time.time())
-        # avoir l'heure actuelles
+        # avoir le jour actuel
         current_day = datetime.datetime.fromtimestamp(current_time).weekday()
-        # supprimer les min et sec + ajouter les heures pour arriver à l'heure demandée
+        # avoir le nombre de jour à ajouter pour arriver au jour demandé
         days_to_add = (days.index(self.__day) - current_day) % 7
         self.__weather = data['daily'][days_to_add]
+        return self.__weather
 
     def __str__(self) -> str:
-        days = ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche']
-        date = datetime.datetime.fromtimestamp(self.__weather["dt"])
-        day = days[date.weekday()]
-        day = " aujourd'hui" if self.__day is None else f" le {day}"
-        hour = "" if self.__hour is None else f" à {self.__hour}h"
-        date = " " + date.strftime("%d/%m/%Y")
-        temperature = self.__weather["temp"]
-        feels_like = self.__weather["feels_like"]
-        humidity = self.__weather["humidity"]
-        description = self.__weather["weather"][0]["description"]
+        if self.__error == "":
+            days = ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche']
+            date = datetime.datetime.fromtimestamp(self.__weather["dt"])
+            day = days[date.weekday()]
+            day = " aujourd'hui" if self.__day is None else f" le {day}"
+            hour = "" if self.__hour is None else f" à {self.__hour}h"
+            date = " " + date.strftime("%d/%m/%Y")
+            description = self.__weather["weather"][0]["description"]
+            temperature = f'{self.__weather["temp"]}°C' if type(self.__weather["temp"]) is float else f"de {self.__weather['temp']['min']}°C à {self.__weather['temp']['max']}°C"
+            feels_like = f'{self.__weather["feels_like"]}°C' if type(self.__weather["feels_like"]) is float else f"de {self.__weather['feels_like']['day']}°C pendant la journée"
+            humidity = self.__weather["humidity"]
 
-        str_to_print = f"Météo à {self.__location}{day}{date}{hour}\n"
-        str_to_print += f"{description}, {temperature}°C avec un ressenti de {feels_like}°C\n"
-        str_to_print += f"{humidity}% d'humidité\n"
+            str_to_print = f"Météo à {self.__location}-{day}{date}{hour}\n"
+            str_to_print += f"{description}, {temperature} avec un ressenti de {feels_like}\n"
+            str_to_print += f"{humidity}% d'humidité\n"
+        else:
+            str_to_print = self.__error
         return str_to_print
